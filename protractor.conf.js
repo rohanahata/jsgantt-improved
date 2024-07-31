bdcdea3cd9dee914c5ee8bdd1a70d7de44b800bb
Rohan Nahata <rohanahata@gmail.com>
2024-07-31 19:21:52 +0530

    cjs to esm

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,8 +1,7 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const { SpecReporter } = require('jasmine-spec-reporter');
-
+import { SpecReporter } from 'jasmine-spec-reporter';
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
